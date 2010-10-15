749a418b280ed4ae8dd7ef03b28e395e0533b529
unknown <pabo@.machcorp.lan>
2010-10-15 12:30:24 +0100

    Inlined some functions and updated code to conform to clojure idiomatic structure

--- test/amazon/test/product.clj
+++ test/amazon/test/product.clj
@@ -4,35 +4,23 @@
   (require [clj-http.client :as client]))
 
 (deftest get-amazon-url-test
-	(let
-		[
-			domain "ecs.amazonaws.com"
+	(let [	domain "ecs.amazonaws.com"
 			access-key "1HWX1AM5RYHK7ZYTBZ82"
 			secret-key "MQ+RXlRIMaGGwLUjHO18C9lqH6ZeFz4oSlMus25Z"
-			associate-id "quote8-21"
-		]
+			associate-id "quote8-21"]
+
 		(testing "Simple item lookup"
-			(let
-				[
-					result
-						(client/get 
-							(get-amazon-url domain access-key secret-key associate-id
-								{:Service "AWSECommerceService", :Operation "ItemLookup", :ItemId "0679722769"}
-							)
-						)
-				]
-				(is	(= 200 (:status result)))
-			)
-		)
+			(let [	result (client/get (get-amazon-url domain access-key secret-key associate-id
+								{:Service "AWSECommerceService", :Operation "ItemLookup", :ItemId "0679722769"}))]
+				(is	(= 200 (:status result)))))
+
 		(testing "Book title search"
-			(is (= 200 (:status (client/get (get-amazon-url domain access-key secret-key associate-id
-				{:Service "AWSECommerceService", :Operation "ItemSearch", :SearchIndex "Books" :Title "Moab is my washpot"}))))
-			)
-		)
+			(let [	result (client/get (get-amazon-url domain access-key secret-key associate-id
+								{:Service "AWSECommerceService", :Operation "ItemSearch", :SearchIndex "Books" :Title "Moab is my washpot"}))]
+				(is	(= 200 (:status result)))))
+
 		(testing "Book title and author search"
-			(is (= 200 (:status (client/get (get-amazon-url domain access-key secret-key associate-id
-				{:Service "AWSECommerceService", :Operation "ItemSearch", :ResponseGroup "Small,Images,Similarities", :SearchIndex "Books" :Title "Moab is my washpot", :Author "Stephen Fry"}))))
-			)
-		)
-	)
-)
+			(let [	result (client/get (get-amazon-url domain access-key secret-key associate-id
+								{:Service "AWSECommerceService", :Operation "ItemSearch", :ResponseGroup "Small,Images,Similarities",
+									:SearchIndex "Books" :Title "Moab is my washpot", :Author "Stephen Fry"}))]
+				(is	(= 200 (:status result)))))))
