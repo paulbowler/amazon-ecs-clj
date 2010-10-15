dac4a3697c8de52c1b55e59da1c56c9dbe0729fd
Paul Bowler <paul_bowler@yahoo.com>
2010-10-15 15:16:51 +0100

    Changed formatting

--- src/amazon/product.clj
+++ src/amazon/product.clj
@@ -6,9 +6,7 @@
 		java.text.SimpleDateFormat
 		javax.crypto.spec.SecretKeySpec
 		javax.crypto.Mac
-		org.apache.commons.codec.binary.Base64
-	)
-)
+		org.apache.commons.codec.binary.Base64))
 
 (def UTF8_CHARSET  "UTF-8")
 (def HMAC_SHA256_ALGORITHM "HmacSHA256")
@@ -16,74 +14,53 @@
 (defn- encodeRfc3986
 	"Manually encodes a string to be RFC3986 compliant"
 	[value]
-	(.replace (.replace (.replace (.replace (.replace (URLEncoder/encode value) "+" "%20") "*" "%2A") "~" "%7E") "," "%2C") ":" "%3A")
-)
-
+	(.replace (.replace (.replace (.replace (.replace (URLEncoder/encode value) "+" "%20") "*" "%2A") "~" "%7E") "," "%2C") ":" "%3A"))
+	
 (defn- encode-signature
 	"Change the signature to encode plus and equal signs"
 	[value]
-	(.replace (.replace value "+" "%2B") "=" "%3D")
-)
+	(.replace (.replace value "+" "%2B") "=" "%3D"))
 
 (defn- map-to-uri
 	"Tranform map to encoded URI string"
 	([uri-map]
-		(apply str (interpose "&" (for [[k v] (sort uri-map)] (str (encodeRfc3986 (name k)) "=" (encodeRfc3986 v)))))
-	)
-)
+		(apply str (interpose "&" (for [[k v] (sort uri-map)] (str (encodeRfc3986 (name k)) "=" (encodeRfc3986 v)))))))
 
 (defn- get-timestamp
 	"Get formatted timestamp for current time"
 	[]
-	(let
-		[
-			cal (Calendar/getInstance)
+	(let [	cal (Calendar/getInstance)
 			dfm (new SimpleDateFormat "yyyy-MM-dd'T'HH:mm:ss'Z'")
-			void (.setTimeZone dfm (TimeZone/getTimeZone  "GMT"))
-		]
-		(.format dfm (.getTime cal))
-	)
-)
+			void (.setTimeZone dfm (TimeZone/getTimeZone  "GMT"))]
+		(.format dfm (.getTime cal))))
 
 (defn- compose-map
 	"Add the access key and associate tag into the map"
 	[uri-map access-key assoc-tag]
-	(conj uri-map {:AWSAccessKeyId access-key, :AssociateTag assoc-tag, :Timestamp (get-timestamp)})
-)
+	(conj uri-map {:AWSAccessKeyId access-key, :AssociateTag assoc-tag, :Timestamp (get-timestamp)}))
 
 (defn- uri-wrapper
 	"Add the required HTTP tokens for colculating the request signature"
 	[domain uri]
-	(str "GET\n" domain "\n/onca/xml\n" uri)
-)
+	(str "GET\n" domain "\n/onca/xml\n" uri))
 
 (defn- sign
 	"Create the signature for the full query string"
 	[string-to-sign secret]
-	(let
-		[
-			secretKeyBytes (.getBytes secret UTF8_CHARSET)
+	(let [	secretKeyBytes (.getBytes secret UTF8_CHARSET)
 			secretKeySpec (new SecretKeySpec secretKeyBytes HMAC_SHA256_ALGORITHM)
 			mac (Mac/getInstance HMAC_SHA256_ALGORITHM)
 			void (.init mac secretKeySpec)
 			data (.getBytes string-to-sign UTF8_CHARSET)
 			rawHmac (.doFinal mac data)
-			encoder (new Base64)
-		]
-		(new String (.encode encoder rawHmac))
-	)
-)
+			encoder (new Base64)]
+		(new String (.encode encoder rawHmac))))
 
 (defn get-amazon-url
 	"Create an encoded and signed URL request compliant with the Amazon Product API"
 	[domain access-key secret assoc-tag params]
-	(let
-		[
-			uri (map-to-uri (compose-map params access-key assoc-tag))
-			signature (encode-signature (sign (uri-wrapper domain uri) secret))
-		]
-		(str "http://" domain "/onca/xml?" uri "&Signature=" signature)
-	)
-)
+	(let [	uri (map-to-uri (compose-map params access-key assoc-tag))
+			signature (encode-signature (sign (uri-wrapper domain uri) secret))]
+		(str "http://" domain "/onca/xml?" uri "&Signature=" signature)))
 		
 		
